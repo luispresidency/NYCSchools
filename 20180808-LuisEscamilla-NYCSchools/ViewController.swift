742a24c59b29b3fe54487fe21315faa3dd5c0d69
Luis Escamilla <luisescamilla@tequilasoft.mx>
2018-08-08 13:27:13 -0400

    Initial Commit

---
+++ 20180808-LuisEscamilla-NYCSchools/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  20180808-LuisEscamilla-NYCSchools
+//
+//  Created by Luis Escamilla on 08/08/18.
+//  Copyright © 2018 Luis Escamilla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
